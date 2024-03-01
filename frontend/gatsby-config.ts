656252de8fc1376e4dada9b36607313cea384294
Adam Zdebel <adam.zdebel1997@gmail.com>
2024-03-01 23:03:03 +0100

    Add initial frontend

---
+++ frontend/gatsby-config.ts
@@ -0,0 +1,58 @@
+import type { GatsbyConfig } from 'gatsby';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL || 'http://127.0.0.1:1337',
+  collectionTypes: ['skill'],
+};
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `ZdebCode`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: strapiConfig,
+    },
+    'gatsby-transformer-remark',
+  ],
+};
+
+export default config;
